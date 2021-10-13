5175b3edecadfaa4d28b716c47c091fddde8cdba
ImranRH321 <imranrh247@gmail.com>
2021-10-14 03:42:01 +0600

    first commit

---
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBD-28_0i5fgWDdqwzke-TuMYWog0fNPrM",
+    authDomain: "genius-car-mechanics-51ec5.firebaseapp.com",
+    projectId: "genius-car-mechanics-51ec5",
+    storageBucket: "genius-car-mechanics-51ec5.appspot.com",
+    messagingSenderId: "105967216359",
+    appId: "1:105967216359:web:e0ce31c756b8022083e442"
+  };
+  
+  
+export default firebaseConfig;
